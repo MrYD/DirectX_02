b9ee27b84b1da0629fe30dccb2300071718e9a58
MrYD <trickroom@gmail.com>
2014-05-31 11:41:19 +0900

    円柱つくりたい

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,59 +19,59 @@ namespace DirectX_01
 {
     public partial class Form1 : Form
     {
-        Device device;
-        SwapChain swapChain;
-        RenderTargetView renderTarget;
 
-        float a = 0;
+        private SwapChain swapChain;
+        private Device device;
+        private RenderTargetView renderTarget;
+        private DepthStencilView depthTarget;
         private Buffer vertexBuffer;
-        private Effect effect;
         private InputLayout inputLayout;
-        private float time;
-        private float area;
-        int n = 1000;
-        public void Render()
-        {
-            effect.GetVariableBySemantic("TIME").AsScalar().Set(time);
-            //   effect.GetVariableBySemantic("AREA").AsScalar().Set(area);
-            a += 0.0003f;
-            //float r = (float)Math.Abs(Math.Sin(Math.PI / 3 + a ));
-            //float g = (float)Math.Abs(Math.Sin(a));
-            //float b = (float)Math.Abs(Math.Sin(-Math.PI / 3 + a));
-            float r = 0.1f;
-            float g = 0.1f;
-            float b = 0.1f;
-            //device.ImmediateContext.ClearRenderTargetView(renderTarget, new Color4(1, (float)Math.Abs(Math.Sin(Math.PI / 3 + a / 1.5)), (float)Math.Abs(Math.Sin(a)), (float)Math.Abs(Math.Sin(Math.PI / 3 - a / 7.5))));//(A,R,G,B)
-            //swapChain.Present(0, PresentFlags.None);
-            device.ImmediateContext.OutputMerger.SetTargets(renderTarget);
-            device.ImmediateContext.ClearRenderTargetView(renderTarget, new Color4(r, g, b));
-            device.ImmediateContext.InputAssembler.InputLayout = inputLayout;
-            device.ImmediateContext.InputAssembler.SetVertexBuffers(0,
-                new VertexBufferBinding[] { new VertexBufferBinding(vertexBuffer, 16, 0) });
-            device.ImmediateContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
-            effect.GetTechniqueByIndex(0).GetPassByIndex(0).Apply(device.ImmediateContext);
-            device.ImmediateContext.Draw(24 * n, 0);
-            swapChain.Present(0, PresentFlags.None);
-            time += 0.001f;
-
-        }
+        private Effect effect;
+        private float time = 0;
+        private int n=100;
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        public void Render()
+        {
+            //描画前の処理
+            device.ImmediateContext.OutputMerger.SetTargets(depthTarget, renderTarget);//描画先の指定
+            device.ImmediateContext.ClearDepthStencilView(depthTarget, DepthStencilClearFlags.Depth, 1f, 0);
+            device.ImmediateContext.ClearRenderTargetView(renderTarget, new Color4(1, 0, 0, 1));//renderTargetを指定色でクリア
+            device.ImmediateContext.InputAssembler.InputLayout = inputLayout;//InputLayoutの指定
+            device.ImmediateContext.InputAssembler.SetVertexBuffers(0,
+                new VertexBufferBinding[] { new VertexBufferBinding(vertexBuffer, 16, 0) });//頂点バッファのセット
+            device.ImmediateContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;//PrimitiveTopologyのセット。今回は三角形リスト
+
+            //最初の描画(薄い紫色)
+            effect.GetVariableBySemantic("COLOR").AsVector().Set(new Vector4(1.0f, 0.8f, 1f, 1f));//float4 col:COLORに対して指定したベクトルをセット
+            effect.GetTechniqueByIndex(0).GetPassByIndex(0).Apply(device.ImmediateContext);//エフェクトの適用
+            device.ImmediateContext.Draw(3, 0);//renderTargetに対して書き込み
+
+            //次の描画(濃い紫色)
+            effect.GetVariableBySemantic("COLOR").AsVector().Set(new Vector4(1f, 0f, 1f, 1f));//float4 col:COLORに対して指定したベクトルをセット
+            effect.GetTechniqueByIndex(0).GetPassByIndex(0).Apply(device.ImmediateContext);//エフェクトの適用
+            device.ImmediateContext.Draw(3, 3);//renderTargetに対して書き込み
+
+            //バックバッファとフロントバッファを入れ替える
+            swapChain.Present(0, PresentFlags.None);
+            time += 0.001f;
+        }
+
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
-            SwapChainDescription desc = new SwapChainDescription() //descの設定
+            SwapChainDescription desc = new SwapChainDescription()
             {
-                BufferCount = 2, // desc.BufferCount=2;と同じ//frontBufferとBackBuffer
+                BufferCount = 2,
                 Flags = SwapChainFlags.AllowModeSwitch,
-                IsWindowed = true,//windowsフォームか
+                IsWindowed = true,
                 ModeDescription = new ModeDescription()
                 {
-                    Format = Format.R8G8B8A8_UNorm,//それぞれ8bit
+                    Format = Format.R8G8B8A8_UNorm,
                     Height = Height,
                     Width = Width,
                     RefreshRate = new Rational(1, 60),
@@ -79,53 +80,60 @@ namespace DirectX_01
                 },
                 OutputHandle = Handle,
                 SampleDescription = new SampleDescription(1, 0),
-                SwapEffect = SwapEffect.Discard,//frontBufferのデータは捨てる
+                SwapEffect = SwapEffect.Discard,
                 Usage = Usage.RenderTargetOutput
             };
-
             Device.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.None,
-                new FeatureLevel[1] { FeatureLevel.Level_10_1 }, desc, out device, out swapChain);
+                new FeatureLevel[1] { FeatureLevel.Level_10_0 }, desc, out device, out swapChain);
             using (Texture2D tex = Texture2D.FromSwapChain<Texture2D>(swapChain, 0))
             {
                 renderTarget = new RenderTargetView(device, tex);
-
             }
-            device.ImmediateContext.Rasterizer.SetViewports(new Viewport[] { new Viewport(0, 0, Right, Bottom, 0, 1) });
-            device.ImmediateContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
-
-            Vector4[] verticie = new Vector4[24 * n];
+            using (Texture2D depthTex = new Texture2D(device, new Texture2DDescription()
+            {
+                ArraySize = 1,
+                BindFlags = BindFlags.DepthStencil,
+                Width = Width,
+                Height = Height,
+                SampleDescription = new SampleDescription(1, 0),
+                Usage = ResourceUsage.Default,
+                Format = Format.D32_Float,
+                CpuAccessFlags = CpuAccessFlags.None,
+                MipLevels = 1,
+                OptionFlags = ResourceOptionFlags.None
+            }))
+            {
+                depthTarget = new DepthStencilView(device, depthTex);
+            }
+            //四角形の頂点を表すリスト
+            Vector4[] verticies = new Vector4[24 * n];
 
             for (int i = 0; i < 24 * n; i = i + 12)
             {
                 float j = i / (6f * n) - 1;
                 float k = (i + 12) / (6f * n) - 1;
                 float mini = 1f;
-                verticie[i] = new Vector4(j, 0, 0, 1);
-                verticie[i + 1] = new Vector4(j, mini, 1, 1);
-                verticie[i + 2] = new Vector4(k, mini, 1, 1);
-                verticie[i + 3] = new Vector4(j, 0, 1, 1);
-                verticie[i + 4] = new Vector4(k, mini, 1, 1);
-                verticie[i + 5] = new Vector4(k, 0, 1, 1);
+                verticies[i] = new Vector4(j, 0, 0, 1);
+                verticies[i + 1] = new Vector4(j, mini, 1, 1);
+                verticies[i + 2] = new Vector4(k, mini, 1, 1);
+                verticies[i + 3] = new Vector4(j, 0, 1, 1);
+                verticies[i + 4] = new Vector4(k, mini, 1, 1);
+                verticies[i + 5] = new Vector4(k, 0, 1, 1);
 
-                verticie[i + 6] = new Vector4(j, 0, 1, 1);
-                verticie[i + 7] = new Vector4(k, -mini, 1, 1);
-                verticie[i + 8] = new Vector4(j, -mini, 1, 1);
-                verticie[i + 9] = new Vector4(j, 0, 1, 1);
-                verticie[i + 10] = new Vector4(k, 0, 1, 1);
-                verticie[i + 11] = new Vector4(k, -mini, 1, 1);
+                verticies[i + 6] = new Vector4(j, 0, 1, 1);
+                verticies[i + 7] = new Vector4(k, -mini, 1, 1);
+                verticies[i + 8] = new Vector4(j, -mini, 1, 1);
+                verticies[i + 9] = new Vector4(j, 0, 1, 1);
+                verticies[i + 10] = new Vector4(k, 0, 1, 1);
+                verticies[i + 11] = new Vector4(k, -mini, 1, 1);
             }
-
-
-
-            using (DataStream ds = new DataStream(verticie, true, true))
+            using (DataStream ds = new DataStream(verticies, true, true))
             {
                 vertexBuffer = new Buffer(device, ds, new BufferDescription()
                 {
                     BindFlags = BindFlags.VertexBuffer,
                     SizeInBytes = (int)ds.Length,
                 });
-
-
             }
             using (ShaderBytecode compiledCode = ShaderBytecode.CompileFromFile("shader.fx", "fx_5_0", ShaderFlags.Debug, EffectFlags.None))
             {
@@ -139,7 +147,6 @@ namespace DirectX_01
                     Format = Format.R32G32B32A32_Float
                 }, 
             });
-            //  device.ImmediateContext.Rasterizer.State=RasterizerState.FromDescription(device,new RasterizerStateDescription(){FillMode=FillMode.Wireframe,CullMode=CullMode.None}); //逆回り
             device.ImmediateContext.Rasterizer.SetViewports(new Viewport[] { new Viewport(0, 0, Width, Height, 0, 1), });
         }
     }
--- bin/Debug/shader.fx
+++ bin/Debug/shader.fx
@@ -1,6 +1,5 @@
-﻿
-float time : TIME;
-
+﻿float time : TIME;
+float4 col:COLOR;
 struct VS_INPUT
 {
 	float4 position:POSITION;
@@ -9,48 +8,25 @@ struct VS_INPUT
 struct PS_INPUT
 {
 	float4 pos:POSITION;
-	float4 position:SV_Position;//ShaderValue
+	float4 position:SV_Position;
 };
-
 
 PS_INPUT VS(VS_INPUT input)
 {
 	PS_INPUT ps;
+	ps.pos = input.position;
 	ps.position = input.position;
-	ps.pos = ps.position;
-	float X = ps.pos.x;
-	float Y = ps.pos.y;
-	float roatX = X*cos(time / 2) - Y*sin(time / 2);
-	float roatY = X*sin(time / 2) + Y*cos(time / 2);
-
-	ps.position.y = sin((time + roatX) * 5);
-	ps.position.x = roatX;
-	
-
 	return ps;
+}
+
+float calcCol(float t, PS_INPUT input)
+{
+	return abs(sin(input.pos.x + t) - input.pos.y)*exp(input.pos.x);
 }
 
 float4 PS(PS_INPUT input) :SV_Target
 {
-	float light = 0.1;
-	float X = input.pos.x;
-	float Y = input.pos.y;
-	float roatX = X*cos(time / 2) - Y*sin(time / 2);
-	float roatY = X*sin(time / 2) + Y*cos(time / 2);
-
-
-	//左に移動するグラデーション
-	//float r = sin((X + time    ) * 2) + X*Y + X * cos(time) + sin(time)*Y + light;     
-	//float g = sin((X + time + 2) * 2) + X*Y + X * cos(time) + cos(time)*Y + light;
-	//float b = sin((X + time - 2) * 2) + Y*Y + X * sin(time) + sin(time)*Y + light;
-
-	//回転しながら弾ける色
-	float r = abs(sin(log(2 + abs(time * roatX / ((roatY + 1) + roatY / (roatX + 1)))))) + sin((roatX + time) * 2) + roatX*roatY + roatX * cos(time) + sin(time)*roatY + light;
-	float g = abs(sin(log(2 + abs(time * roatX / (roatY + 1))))) + sin((roatX + time + 2) * 2) + roatX*roatY + roatX * cos(time) + cos(time)*roatY + light;
-	float b = abs(sin(log(2 + abs(time * roatY / (roatX + 1))))) + sin((roatX + time - 2) * 2) + roatY*roatY + roatX * sin(time) + sin(time)*roatY + light;
-
-
-	return float4(r, g, b, 1);
+	return col;
 }
 
 technique10 DefaultTechnique
